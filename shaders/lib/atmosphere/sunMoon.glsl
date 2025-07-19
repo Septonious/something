84b49fccd4b9534b01a47e032c67464ec8a73d90
Septonious <83358692+Septonious@users.noreply.github.com>
2025-07-19 15:44:10 +0300

    Update sunMoon.glsl

--- shaders/lib/atmosphere/sunMoon.glsl
+++ shaders/lib/atmosphere/sunMoon.glsl
@@ -9,18 +9,18 @@ void drawSunMoon(inout vec3 color, in vec3 worldPos, in vec3 nViewPos, in float 
               moon = float(moon > 0.0) * 4.0 * moonVisibility;
         float glare = pow32(VoS * sunVisibility + VoM * moonVisibility) * 0.25;
 
-		if (0 < moon && 0 < moonPhase) { // Moon phases, uses the same method as Complementary v4
-			float phaseFactor = int(moonPhase != 4) * (1.0 - int(4 < moonPhase) * 2.0) * 0.00175;
-
-            const vec2 sunRotationData = vec2(cos(sunPathRotation * 0.01745329251994), -sin(sunPathRotation * 0.01745329251994));
-            float fractTimeAngle = fract(timeAngle - (0.25 + phaseFactor));
-            float ang = (fractTimeAngle + (cos(fractTimeAngle * 3.14159265358979) * -0.5 + 0.5 - fractTimeAngle) / 3.0) * 6.28318530717959;
-            vec3 newSunVec = normalize((gbufferModelView * vec4(vec3(-sin(ang), cos(ang) * sunRotationData) * 2000.0, 1.0)).xyz);
-
-			moon *= 0.05 + clamp(1.0 - max(pow32(pow32(dot(nViewPos, newSunVec))) - 0.4, 0.0) * 14.0, 0.0, 1.0);
-		}
-
         if (moon > 0.0) {
+            if (moonPhase > 0) { // Moon phases, uses the same method as Complementary v4
+                float phaseFactor = int(moonPhase != 4) * (1.0 - int(4 < moonPhase) * 2.0) * 0.00175;
+
+                const vec2 sunRotationData = vec2(cos(sunPathRotation * 0.01745329251994), -sin(sunPathRotation * 0.01745329251994));
+                float fractTimeAngle = fract(timeAngle - (0.25 + phaseFactor));
+                float ang = (fractTimeAngle + (cos(fractTimeAngle * 3.14159265358979) * -0.5 + 0.5 - fractTimeAngle) / 3.0) * 6.28318530717959;
+                vec3 newSunVec = normalize((gbufferModelView * vec4(vec3(-sin(ang), cos(ang) * sunRotationData) * 2000.0, 1.0)).xyz);
+
+                moon *= 0.05 + clamp(1.0 - max(pow32(pow32(dot(nViewPos, newSunVec))) - 0.4, 0.0) * 14.0, 0.0, 1.0);
+            }
+
             worldPos = normalize(worldPos);
             vec2 planeCoord = worldPos.xz / (worldPos.y + length(worldPos));
             moon *= texture2D(noisetex, planeCoord * 1.5).r * 0.65 + 0.35;
