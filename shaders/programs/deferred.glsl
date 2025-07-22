1b8392a5f67d784cff36c46e426ada7f4ed79eba
Septonious <83358692+Septonious@users.noreply.github.com>
2025-07-22 12:22:07 +0300

    sky and ispalegarden fix

--- shaders/programs/deferred.glsl
+++ shaders/programs/deferred.glsl
@@ -14,6 +14,10 @@ uniform int moonPhase;
 uniform float timeAngle, timeBrightness, wetness;
 
 uniform float isLushCaves, isDesert;
+
+#if MC_VERSION >= 12104
+uniform float isPaleGarden;
+#endif
 #endif
 
 uniform float far, near;
