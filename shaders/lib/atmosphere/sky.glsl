58c2b9227c51e7556eea86f72e439dba99d2c8d1
Septonious <septonious@proton.me>
2025-07-31 14:20:51 +0300

    improved atmospherics

--- shaders/lib/atmosphere/sky.glsl
+++ shaders/lib/atmosphere/sky.glsl
@@ -1,29 +1,35 @@
 vec3 getAtmosphere(vec3 viewPos, vec3 worldPos) {
      vec3 nViewPos = normalize(viewPos);
 
-     float VoSRaw = dot(nViewPos, sunVec);
-     float VoURaw = dot(nViewPos, upVec);
-     float VoUClamped = clamp(VoURaw, 0.0, 1.0);
-     float VoSClamped = clamp(VoSRaw, 0.0, 1.0);
-           VoSClamped = pow(VoSClamped, 1.25);
+     float VoS = dot(nViewPos, sunVec);
+     float VoU = dot(nViewPos, upVec);
+     float VoSPositive = VoS * 0.5 + 0.5;
+     float VoUPositive = VoU * 0.5 + 0.5;
+     float VoSClamped = clamp(VoS, 0.0, 1.0);
+     float VoUClamped = clamp(VoU, 0.0, 1.0);
 
-     float skyDensity = exp((-0.6 + timeBrightness * 0.3) * abs(VoURaw));
-     float baseScatteringHeight = pow6(1.0 - VoUClamped);
-     float horizonScatteringHeight = pow2(1.0 - VoUClamped);
+     float daySkyDensity = 0.5 + 0.5 * exp(-2.5 * VoUPositive * VoUPositive + 0.5 * timeBrightness);
+	float nightSkyDensity = exp(-0.7 * VoUClamped);
+     float skyDensity = mix(nightSkyDensity, daySkyDensity, timeBrightnessSqrt);
 
      //Fake light scattering
-     vec3 scattering = mix(vec3(2.7, 2.7, 0.1), vec3(1.9, 0.8, 0.0), pow2(1.0 - VoUClamped));
-          scattering += vec3(0.8, 0.1, 0.2) * pow12(VoSClamped) * (1.0 - timeBrightnessSqrt);
-          scattering = pow(scattering, vec3(1.5));
-          scattering *= pow4(1.0 - abs(VoURaw));
-          scattering *= 0.5 * exp(VoSRaw * 0.75);
+     float mieScattering = 0.25 * exp(pow3(VoSClamped));
+     VoUClamped = max(VoUClamped - (0.15 - VoSPositive * 0.15) * (1.0 - sunVisibility), 0.0);
+     float rayleighScatteringMixer = pow(VoUClamped, 0.35);
+     vec3 rayleighScattering = mix(vec3(8.8, 1.2, 0.0), vec3(4.0, 5.0, 1.0), rayleighScatteringMixer);
+          rayleighScattering = mix(rayleighScattering, lightColSqrt * 2.0, VoUPositive * VoUPositive);
+          rayleighScattering *= pow(VoUClamped, 1.3 - VoSPositive * 0.4) * pow2(1.0 - VoUClamped);
+          rayleighScattering *= (0.6 - sunVisibility * 0.3) * exp(VoSPositive);
+
+     float sunScattering = sunVisibility * pow(1.0 - VoUClamped, 5.0 - VoSClamped * 3.0) * (0.4 + VoSClamped * VoSClamped * 0.25);
 
      vec3 nSkyColor = normalize(skyColor + 0.000001) * mix(vec3(1.0), biomeColor, isSpecificBiome);
-     vec3 daySky = mix(nSkyColor, vec3(0.6, 0.45, 0.5), 0.5 - timeBrightnessSqrt * 0.5);
-          daySky += scattering * pow2(max(0.0, 0.5 - timeBrightnessSqrt)) * (1.0 - wetness);
-          daySky = mix(daySky, lightColSqrt, baseScatteringHeight * exp(VoSRaw * 0.5) * 0.5);
-          daySky = mix(daySky, lightColSqrt, timeBrightnessSqrt * horizonScatteringHeight * VoSClamped * 0.5);
-     vec3 nightSky = lightNight * 0.75;
+     vec3 daySky = mix(nSkyColor, vec3(0.85, 0.75, 1.05), 0.4 - timeBrightnessSqrt * 0.4);
+          //daySky *= 1.0 + mieScattering;
+          daySky += rayleighScattering * (0.6 - timeBrightness * 0.3) * (1.0 - wetness);
+          daySky = mix(daySky, lightColSqrt, sunScattering);
+
+     vec3 nightSky = lightNight * 0.65;
      vec3 atmosphere = mix(nightSky, daySky, sunVisibility);
           atmosphere *= 1.0 - wetness * 0.125;
           atmosphere *= skyDensity;
