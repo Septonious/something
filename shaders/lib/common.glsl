c36d578a4d843b3db97587762e159756c5eb2ded
Septonious <septonious@proton.me>
2025-09-21 19:56:15 +0300

    enable vx lpv

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -601,9 +601,6 @@ vec3 pow8(vec3 x) {return x*x*x*x*x*x*x*x;}
 #define VX_SUPPORT
 #endif
 
-#undef VX_SUPPORT
-#undef DYNAMIC_HANDLIGHT //REMOVE
-
 #if defined DH_TERRAIN || defined DH_WATER
 #undef DYNAMIC_HANDLIGHT
 #endif
